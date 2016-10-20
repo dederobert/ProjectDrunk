af1b8177eb9c7cd30834079058caebcbd341ae1a
Dederobert <dederoberthd@gmail.com>
2016-10-20 11:32:56 +0200

    creation vendor

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * File index.php
+ *
+ * PHP version 5
+ * @category www
+ * @package Root
+ * @link /
+ * @since Version 0.0.1
+ * @version 0.0.1
+ */ 
+require join(DIRECTORY_SEPARATOR, array(dirname(__DIR__), 'vendor', 'autoload.php'));
+
+Drunk\Core\Drunk::start();
+?>
---
+++ vendor/Drunk/Controller/Controller.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+* 
+*/
+namespace Drunk\Controller;
+class Controller
+{
+
+}
+ ?>
