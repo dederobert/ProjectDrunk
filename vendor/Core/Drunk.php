99d416fbc32ac89eb90293888d73a04526ae7ef5
Dederobert <dederoberthd@gmail.com>
2016-10-20 11:41:09 +0200

    Autoload

---
+++ vendor/Core/Drunk.php
@@ -0,0 +1,20 @@
+<?php 
+/**
+* File Drunk.php
+*
+* PHP version 
+* @category Core
+* @package Core
+* @link /
+* @since Version 0.0.1
+* @version 0.0.1
+*/
+namespace Drunk\Core;
+/**
+* 
+*/
+class Drunk
+{
+	
+}
+ ?>
---
+++ vendor/autoload.php
@@ -0,0 +1,19 @@
+<?php 
+/**
+* File autoload.php
+*
+* PHP version 5
+* @category Autoload
+* @package Drunk
+* @link /
+* @since Version 0.0.1
+* @version 0.0.1
+*/
+define('DS', DIRECTORY_SEPARATOR);
+define('CONF_PATH', join(DS, array(dirname(__DIR__), 'config'));
+
+spl_autoload_register(function($class)
+{
+	include $class . '.php';
+});
+ ?>
