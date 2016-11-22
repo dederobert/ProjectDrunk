84f69e7fb0376fb671af0166a2cf0da9cceddc1f
Dederobert <dederoberthd@gmail.com>
2016-11-22 15:07:01 +0100

    efe

--- parts/Models/Ingredients.php
+++ parts/Models/Ingredients.php
@@ -22,16 +22,37 @@ use DDM\Core\Source\File;
 class Ingredients extends Model
 {
 	private $file; 
+	private $hierarchie;
+
 	public function init()
 	{
 		$ddm = DrunkDataManager::getInstance();
 		$this->file = $ddm->load(new File("Donnees.inc.php"), false);
-
+		$hierarchie = $this->file->read()['Hierarchie'];
 	}
 
 	public function getAll()
 	{
-		return $this->file->read()['Hierarchie'];
+		$ingredients = array();
+		
+		foreach ($hierarchie as $ingredient) {
+			$ingredients[] = $ingredient;
+		}
+		return $ingredients;
 	}
+
+	public function get($ingredient)
+	{
+		if (isset($hierarchie[$ingredient])) {
+			return $hierarchie[$ingredient];
+		}
+		return false;
+	}
+
+	public function getHierarchie($ingredientName)
+	{
+		
+	}
+
 }
  ?>
