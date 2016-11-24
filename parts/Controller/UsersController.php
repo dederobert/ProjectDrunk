764e216a59e7d5b3660d02b516a52fdbcf9f61ac
Dederobert <dederoberthd@gmail.com>
2016-11-24 19:34:34 +0100

    UserController

---
+++ parts/Controller/UsersController.php
@@ -0,0 +1,37 @@
+<?php 
+/**
+ * File UsersController.php
+ *
+ * PHP version 7
+ * @category Controller
+ * @package Controller
+ * @license http://opensource.org/licenses/MIT
+ * @link /
+ * @since Version 0.0.1
+ * @version 0.0.1
+ */
+namespace App\Controller;
+
+use Drunk\Controller\Controller;
+/**
+* 
+*/
+class UsersController extends Controller
+{
+	
+	public function login()
+	{
+		
+	}
+
+	public function logout()
+	{
+		
+	}
+
+	public function register()
+	{
+		
+	}
+}
+  ?>
